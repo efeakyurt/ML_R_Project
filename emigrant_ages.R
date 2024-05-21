719b929f038786dc0a7c25961e16ca8d51ca83c7
efeakyurt <88876644+efeakyurt@users.noreply.github.com>
2024-05-21 14:40:31 +0300

    Update emigrant_ages.R

--- emigrant_ages.R
+++ emigrant_ages.R
@@ -1,29 +1,29 @@
 library(readxl)
 library(dplyr)
 
-# Load the dataset
-file_path <- '/Users/efeakyurt/Desktop/ML_R_Project/immigrants_emigrants_age_group_and_sex.xlsx'
-data <- read_excel(file_path, skip = 4) # Skip first four rows directly
 
-# Renaming the columns for easier access
+file_path <- '/Users/efeakyurt/Desktop/ML_R_Project/immigrants_emigrants_age_group_and_sex.xlsx'
+data <- read_excel(file_path, skip = 4) 
+
+
 colnames(data) <- c('Year', 'Age Group', 'Total Population', 'Immigrants Total', 'Immigrants Male', 
                     'Immigrants Female', 'Unknown Column', 'Emigrants Total', 'Emigrants Male', 'Emigrants Female')
 
-# Dropping the 'Unknown Column' as it appears to be empty
-data_cleaned <- data %>% select(-`Unknown Column`)
 
-# Converting 'Year' to character for later operations
+data_cleaned <- data %>% select(-`Unknown Column`)  #There was an 'Unknown Column' in the data, so I am droping it
+
+
 data_cleaned$Year <- as.character(data_cleaned$Year)
 
-# Fill missing 'Year' values by propagating the last valid observation forward
-data_cleaned <- data_cleaned %>% tidyr::fill(Year, .direction = "down")
 
-# Modify 'Age Group' to include the year, format it as requested
+data_cleaned <- data_cleaned %>% tidyr::fill(Year, .direction = "down") # Filling the relevant 'Year' values for each age group
+
+
 data_cleaned$`Age Group` <- paste(data_cleaned$`Age Group`, "(", data_cleaned$Year, ")", sep = "")
 
-# Selecting the relevant columns as per the requirements
-final_data <- data_cleaned %>% select(Year, `Age Group`, `Emigrants Total`, `Emigrants Male`, `Emigrants Female`)
 
-# Print first few rows of the final data
+final_data <- data_cleaned %>% select(Year, `Age Group`, `Emigrants Total`, `Emigrants Male`, `Emigrants Female`) #Then I have selected the columns I need
+
+
 head(final_data)
 view(final_data)
